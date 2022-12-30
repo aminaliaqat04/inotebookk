f51d0604bcebe7872f7e98a489ed1564a6bcef7a
Amina Liaqat <71599939+aminaliaqat04@users.noreply.github.com>
2022-12-30 12:22:03 +0500

    full stack

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const url = `http://localhost:5000/api/auth/login`;
+        const url = `/api/auth/login`;
         const response = await fetch(url, {
             method: "POST",
             headers: {
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
